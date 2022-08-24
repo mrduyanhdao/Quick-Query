41582e135d2e3f987210db198dfa3f0bd65605cb
Duy Anh <anhdd@sendo.vn>
2022-08-24 15:04:49 +0700

    Update query

--- result/Query_Firebase.sql
+++ result/Query_Firebase.sql
@@ -88,40 +88,11 @@ With click as
        
     
     
-       
-      and (select value.string_value from unnest (event_params) where key = 'source_block_id' ) not in ("og_ingredients","og_ingredient")
-       
-    
   
 
 
 
 
-
-    UNION ALL 
-  
-    SELECT 
-    'Purchase' as EventName,
-     CASE WHEN (SELECT value.string_value from unnest (user_properties) where key in ("firebase_exp_288","firebase_exp_289") ) = '0' then 'Baseline'
-     
-     when (SELECT value.string_value from unnest (user_properties) where key in ("firebase_exp_288","firebase_exp_289") ) = '1' then 'Experiment 1 ' 
-       
-     when (SELECT value.string_value from unnest (user_properties) where key in ("firebase_exp_288","firebase_exp_289") ) = '2' then 'Experiment 2 ' 
-       
-     when (SELECT value.string_value from unnest (user_properties) where key in ("firebase_exp_288",  "firebase_exp_289") ) = '3' then 'Experiment 3 ' 
-       
-    END AS ABType ,
-    event_date, 
-     platform,
-     user_pseudo_id
-    FROM sendo-data-platform.b401_ha_firebasebuyer.trf_b201_og_purchase
-    where 1 =1 
-    and event_date >= '2022-08-10'
-    and event_date <= '2022-08-22'
-    and (SELECT key from unnest(user_properties) where key in ("firebase_exp_288","firebase_exp_289") ) is not null 
-    
-    
-  
 
 
 ),
